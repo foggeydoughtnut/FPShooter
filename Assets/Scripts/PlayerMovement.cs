698b11444b67bb6fa0ec1bfe98b5d3996dc7f3df
foggeydoughtnut <foggeydoughtnut@gmail.com>
2022-01-30 15:07:58 -0700

    Added some acceleration to the walking

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,6 +6,9 @@ public class PlayerMovement : MonoBehaviour
 {
     public CharacterController controller;
     public float speed = 12f;
+    public float accelerationIncrease = 0.1f;
+    public float acceleration = 1f;
+    public float maxAcceleration = 1.6f;
     public float gravity = -9.81f;
     public float jumpHeight = 3f;
 
@@ -15,19 +18,40 @@ public class PlayerMovement : MonoBehaviour
 
     Vector3 velocity;
     bool isGrounded;
+    float defaultAcceleration;
 
+    private void Start()
+    {
+        defaultAcceleration = acceleration;
+    }
 
     // Update is called once per frame
     void Update()
     {
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
+
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
         Vector3 move = transform.right * x + transform.forward * z;
 
-        controller.Move(move * speed * Time.deltaTime);
+        if (move != Vector3.zero)
+        {
+            if (acceleration <= maxAcceleration)
+            {
+                acceleration += accelerationIncrease;
+            } else
+            {
+                acceleration = maxAcceleration;
+            }
+        } else
+        {
+            acceleration = defaultAcceleration;
+        }
+
+        controller.Move(move * speed * acceleration * Time.deltaTime);
+        
 
         if (Input.GetButtonDown("Jump") && isGrounded)
         {
